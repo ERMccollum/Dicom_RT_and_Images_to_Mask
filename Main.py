b1a86e013dee02a6e1a8b59a99e2fac8ed7e817c
breber <brandon.reber@alumni.ubc.ca>
2020-06-17 20:35:05 -0500

    After modifying the dose_resnet_run file for the various runs

--- Main.py
+++ Main.py
@@ -27,3 +27,23 @@ if write_files:
 '''
 Distribute the nifti files to other folders
 '''
+distribute = False
+if distribute:
+    from Pre_Processing.Dicom_RT_and_Images_to_Mask.Distribute_Train_Test_Validation import distribute
+    distribute('HN',niftii_path=nifti_path, excel_file=os.path.join('.','MRN_Path_To_Iteration.xlsx'))
+
+'''
+Now that they're distributed, turn them into the tfrecord structure
+# '''
+make_single_images = True
+nifti_path = r'I:\Morfeus\BReber\HN_Status_Pred\Nifti_Files'
+if make_single_images:
+    from Pre_Processing.Make_Single_Images.Make_TFRecord_Class import write_tf_record
+    from Pre_Processing.Make_Single_Images.Image_Processors_Module.Image_Processors_TFRecord import *
+    image_processors = [Add_Dose(), Clip_Images_By_Extension(extension=16), Distribute_into_3D(min_z=64, max_z=64, mirror_small_bits=True)] #, Add_Dose(), Sum_Dose(),
+    write_tf_record(os.path.join(nifti_path, 'Train'), record_name='Train', image_processors=image_processors,
+                      is_3D=True, rewrite=False, thread_count=5)
+    # write_tf_record(os.path.join(nifti_path, 'Validation'), record_name='Validation', image_processors=image_processors,
+    #                   is_3D=True, rewrite=True, thread_count=5)
+    # write_tf_record(os.path.join(nifti_path, 'Test'), record_name='Test', image_processors=image_processors,
+    #                   is_3D=True, rewrite=True, thread_count=5)
